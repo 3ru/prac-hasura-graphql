32c2d3574b7a3beb369afdde8f6b0895723ce269
Ryuyxx <Ryuyxx@gmail.com>
2021-09-20 16:09:53 +0900

    :tada: init

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import { Layout } from '../components/Layout'
+import { VFC } from 'react'
+
+const Home: VFC = () => {
+  return (
+    <Layout title="Home">
+      <p className="text-3xl font-bold">Next.js + GraphQL</p>
+    </Layout>
+  )
+}
+
+export default Home
