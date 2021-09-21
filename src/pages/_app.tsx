10ac29dcd66364231c72623853e6f96a3ec6cfe4
Ryuyxx <Ryuyxx@gmail.com>
2021-09-21 21:19:30 +0900

    fix: tailwind config

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import '../styles/globals.css'
+// import '../../styles/globals.css'
 import { AppProps } from 'next/app'
 import { ApolloProvider } from '@apollo/client'
 import { initializeApollo } from '../lib/apolloClient'
